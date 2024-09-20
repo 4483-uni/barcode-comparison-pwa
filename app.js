cd87a4257f544f80aa4113abb4177fb390a4795a
4483-uni <hoshiya@unitechsys.co.jp>
2024-09-20 15:15:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,133 @@
-// app.js
-// 新しい検出器の生成
-const barcodeDetector = new BarcodeDetector({
-  formats: ["code_39", "codabar", "ean_13"],
-});
+window.SQR = window.SQR || {}
 
-// 互換性のチェック
-if (barcodeDetector) {
-  alert('Barcode Detector に対応しています。');
-} else {
-  alert('Barcode Detector はこのブラウザーでは対応していません。');
-}
-
-/*
-// スキャン開始ボタンのクリックイベント
-document.getElementById('start-scan').addEventListener('click', function() {
-    // 対応ブラウザかチェック
-    if ('BarcodeDetector' in window) {
-        // 対応するバーコード形式を指定
-        const barcodeDetector = new BarcodeDetector({ formats: ['data_matrix'] });
-        const videoElement = document.getElementById('video');
-
-        // スキャン中の表示を開始
-        document.getElementById('scanning-indicator').style.display = 'block';
-
-        // カメラの映像を取得
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-            .then(stream => {
-                videoElement.srcObject = stream;
-                videoElement.play();
-
-                // スキャン関数の定義
-                const scanBarcode = () => {
-                    barcodeDetector.detect(videoElement)
-                        .then(barcodes => {
-                            if (barcodes.length > 0) {
-                                // バーコードが検出された場合
-                                document.getElementById('barcode1').value = barcodes[0].rawValue;
-
-                                // スキャン中の表示を停止
-                                document.getElementById('scanning-indicator').style.display = 'none';
-
-                                // カメラストリームを停止
-                                stream.getTracks().forEach(track => track.stop());
-                            } else {
-                                // 再度スキャンを試みる
-                                requestAnimationFrame(scanBarcode);
-                            }
-                        })
-                        .catch(err => {
-                            console.error(err);
-                            alert('バーコードの検出中にエラーが発生しました。');
-
-                            // スキャン中の表示を停止
-                            document.getElementById('scanning-indicator').style.display = 'none';
-
-                            // カメラストリームを停止
-                            stream.getTracks().forEach(track => track.stop());
-                        });
-                };
-
-                // スキャンを開始
-                scanBarcode();
-            })
-            .catch(err => {
-                console.error(err);
-                alert('カメラへのアクセスが拒否されました。');
-                // スキャン中の表示を停止
-                document.getElementById('scanning-indicator').style.display = 'none';
-            });
-    } else {
-        alert('このブラウザはBarcode Detector APIをサポートしていません。最新のChromeまたはEdgeをご利用ください。');
+SQR.reader = (() => {
+    /**
+     * getUserMedia()に非対応の場合は非対応の表示をする
+     */
+    const showUnsuportedScreen = () => {
+        document.querySelector('#js-unsupported').classList.add('is-show')
     }
-});
-*/
+    if (!navigator.mediaDevices) {
+        showUnsuportedScreen()
+        return
+    }
+
+    const video = document.querySelector('#js-video')
+
+    /**
+     * videoの出力をCanvasに描画して画像化 jsQRを使用してQR解析
+     */
+    const checkQRUseLibrary = () => {
+        const canvas = document.querySelector('#js-canvas')
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        const code = jsQR(imageData.data, canvas.width, canvas.height)
+
+        if (code) {
+            SQR.modal.open(code.data)
+        } else {
+            setTimeout(checkQRUseLibrary, 200)
+        }
+    }
+
+    /**
+     * videoの出力をBarcodeDetectorを使用してQR解析
+     */
+    const checkQRUseBarcodeDetector = () => {
+        const barcodeDetector = new BarcodeDetector()
+        barcodeDetector
+            .detect(video)
+            .then((barcodes) => {
+                if (barcodes.length > 0) {
+                    for (let barcode of barcodes) {
+                        SQR.modal.open(barcode.rawValue)
+                    }
+                } else {
+                    setTimeout(checkQRUseBarcodeDetector, 200)
+                }
+            })
+            .catch(() => {
+                console.error('Barcode Detection failed, boo.')
+            })
+    }
+
+    /**
+     * BarcodeDetector APIを使えるかどうかで処理を分岐
+     */
+    const findQR = () => {
+        window.BarcodeDetector
+            ? checkQRUseBarcodeDetector()
+            : checkQRUseLibrary()
+    }
+
+    /**
+     * デバイスのカメラを起動
+     */
+    const initCamera = () => {
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: false,
+                video: {
+                    facingMode: {
+                        exact: 'environment',
+                    },
+                },
+            })
+            .then((stream) => {
+                video.srcObject = stream
+                video.onloadedmetadata = () => {
+                    video.play()
+                    findQR()
+                }
+            })
+            .catch(() => {
+                showUnsuportedScreen()
+            })
+    }
+
+    return {
+        initCamera,
+        findQR,
+    }
+})()
+
+SQR.modal = (() => {
+    const result = document.querySelector('#js-result')
+    const link = document.querySelector('#js-link')
+    const copyBtn = document.querySelector('#js-copy')
+    const modal = document.querySelector('#js-modal')
+    const modalClose = document.querySelector('#js-modal-close')
+
+    /**
+     * 取得した文字列を入れ込んでモーダルを開く
+     */
+    const open = (url) => {
+        result.value = url
+        link.setAttribute('href', url)
+        modal.classList.add('is-show')
+    }
+
+    /**
+     * モーダルを閉じてQR読み込みを再開
+     */
+    const close = () => {
+        modal.classList.remove('is-show')
+        SQR.reader.findQR()
+    }
+
+    const copyResultText = () => {
+        result.select()
+        document.execCommand('copy')
+    }
+
+    copyBtn.addEventListener('click', copyResultText)
+
+    modalClose.addEventListener('click', () => close())
+
+    return {
+        open,
+    }
+})()
+
+if (SQR.reader) SQR.reader.initCamera()
