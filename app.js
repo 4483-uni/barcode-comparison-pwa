f72d80969ba2f5342dcb879f4388b5fbebf7374a
4483-uni <hoshiya@unitechsys.co.jp>
2024-09-20 10:18:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ document.getElementById('compare').addEventListener('click', function() {
 document.getElementById('start-scan').addEventListener('click', function() {
     const hints = new Map();
     hints.set(ZXing.DecodeHintType.POSSIBLE_FORMATS, [ZXing.BarcodeFormat.DATA_MATRIX]);
-    //hints.set(ZXing.DecodeHintType.TRY_HARDER, true); // 認識精度を上げる
-    hints.set(ZXing.DecodeHintType.PURE_BARCODE, true); // PURE_BARCODEヒントを試す
+    hints.set(ZXing.DecodeHintType.TRY_HARDER, true); // 認識精度を上げる
+    //hints.set(ZXing.DecodeHintType.PURE_BARCODE, true); // PURE_BARCODEヒントを試す
     
     const codeReader = new ZXing.BrowserMultiFormatReader(hints);
     //const codeReader = new ZXing.BrowserMultiFormatReader();
